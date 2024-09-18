2d8784dc22d13f14598aea9235fdd53e092a0d24
Aleksandr <aleksdark13@gmail.com>
2024-09-18 20:59:43 +0300

    logging

--- show_users.py
+++ show_users.py
@@ -1,21 +1,13 @@
 from peewee import SqliteDatabase, Model, CharField
+from models import User
 
 # Assuming your database file is named 'mydatabase.db'
 db = SqliteDatabase('history.db')
-
-
-class User(Model):
-    email = CharField()
-    password = CharField()
-
-    class Meta:
-        database = db
-
 
 db.connect()
 users = User.select()
 
 for user in users:
-    print(f'Email: {user.email}, Password: {user.password}')
+    print(f'Email: {user.email}, Password: {user.password}, User name {user.user_name}')
 
 db.close()
